8a94ecff7db8e4e20e6e160c6e1a87f2880849f1
dantesingleton <dante.singleton@gmail.com>
2018-09-07 15:21:38 -0700

    Assorted fixes

--- lib/index.ts
+++ lib/index.ts
@@ -4,13 +4,6 @@ export * from './Builder';
 export * from './EventDispatcher';
 export * from './factory';
 export * from './Model';
-// export * from './helpers';
+export * from './helpers';
 export * from './Schema'
 export let tang = new Tang()
-
-// tang.Builder = Builder
-// tang.EventDispatcher = EventDispatcher
-// tang.factory = factory
-// tang.Model = Model
-// tang.Schema = Schema
-// tang.helpers = helpers
